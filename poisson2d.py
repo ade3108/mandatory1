a84ed4984f837f5354e3cdfb8d4b00aea8682bd4
ade3108 <adeliab@uio.no>
2024-10-02 10:28:52 +0200

    maybe

--- poisson2d.py
+++ poisson2d.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sympy as sp
 import scipy.sparse as sparse
+from scipy.interpolate import RectBivariateSpline
 
 x, y = sp.symbols('x,y')
 
@@ -32,29 +33,66 @@ class Poisson2D:
 
     def create_mesh(self, N):
         """Create 2D mesh and store in self.xij and self.yij"""
-        # self.xij, self.yij ...
-        raise NotImplementedError
+        self.N=N
+        self.h=self.L/N
+        xi = np.linspace(0, self.L, N+1)  # x coordinates
+        yj = np.linspace(0, self.L, N+1)  # y coordinates
+        self.xij, self.yij = np.meshgrid(xi, yj, indexing='ij')
+        return self.xij, self.yij
 
     def D2(self):
         """Return second order differentiation matrix"""
-        raise NotImplementedError
+        D = sparse.diags([1, -2, 1], [-1, 0, 1], (self.N+1, self.N+1), 'lil')
+        D[0, :4] = 2, -5, 4, -1
+        D[-1, -4:] = -1, 4, -5, 2
+        D /= self.h**2
+        return D
 
     def laplace(self):
         """Return vectorized Laplace operator"""
-        raise NotImplementedError
+        D2x = self.D2()
+        D2y = self.D2()
+        Ix = sparse.eye(self.N+1)
+        Iy = sparse.eye(self.N+1)
+        return (sparse.kron(D2x, Iy) + sparse.kron(Ix, D2y)).tolil()
 
     def get_boundary_indices(self):
         """Return indices of vectorized matrix that belongs to the boundary"""
-        raise NotImplementedError
+        B = np.ones((self.N+1, self.N+1), dtype=bool)
+        B[1:-1, 1:-1] = 0
+        return np.where(B.ravel() == 1)[0]
+    
+    def meshfunction(self, u):
+        """Return Sympy function as mesh function
+
+        Parameters
+        ----------
+        u : Sympy function
+
+        Returns
+        -------
+        array - The input function as a mesh function
+        """
+        return sp.lambdify((x, y), u)(self.xij, self.yij)
 
     def assemble(self):
         """Return assembled matrix A and right hand side vector b"""
-        # return A, b
-        raise NotImplementedError
+        A = self.laplace()
+        bnds = self.get_boundary_indices()
+        for i in bnds:
+            A[i] = 0
+            A[i, i] = 1
+        A = A.tocsr()
+        b = np.zeros((self.N+1, self.N+1))
+        b[:, :] = self.meshfunction(self.f)
+        # Set boundary conditions
+        uij = self.meshfunction(self.ue)
+        b.ravel()[bnds] = uij.ravel()[bnds]
+        return A, b
 
     def l2_error(self, u):
         """Return l2-error norm"""
-        raise NotImplementedError
+        return np.sqrt(self.h**2*np.sum((u - self.meshfunction(self.ue))**2))
 
     def __call__(self, N):
         """Solve Poisson's equation.
@@ -113,7 +151,8 @@ class Poisson2D:
         The value of u(x, y)
 
         """
-        raise NotImplementedError
+        spline= RectBivariateSpline(np.linspace(0,self.L, self.N+1),np.linspace(0,self.L, self.N+1), self.U)
+        return spline(x,y)[0,0]
 
 def test_convergence_poisson2d():
     # This exact solution is NOT zero on the entire boundary
